d2a1e8497c28617e06552150c78856df3fcbe627
dusdn022 <dusdn0111@naver.com>
2022-10-01 05:26:56 +0900

    Member, Review Cotroller만 작성 Member 로그인, 회원가입, 회원정보 변경, 회원탈퇴 Review 후기 작성, 수정, 삭제 미흡한 부분 수정 예정

--- SpringShoes/src/main/java/com/ss/biz/controller/MemberController.java
+++ SpringShoes/src/main/java/com/ss/biz/controller/MemberController.java
@@ -1,5 +1,50 @@
 package com.ss.biz.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ss.biz.member.MemberVO;
+import com.ss.biz.member.impl.MemberDAO;
+
+@Controller
 public class MemberController {
 
+	
+	@RequestMapping(value="/login.do", method=RequestMethod.POST)
+	public String SelectOneMember(MemberVO mVO, MemberDAO mDAO, Model model) {
+		
+		mVO = mDAO.selectOneMember(mVO);	
+		if(mVO==null) {		// 로그인 실패시
+			return "redirect:login.jsp"; // 다시 로그인 화면으로 이동
+		}
+		else {
+			model.addAttribute("member", mVO); //로그인 성공시
+			return "";	//메인화면으로 이동  main?
+		}
+	}
+	
+	@RequestMapping(value="/insertMember.do", method=RequestMethod.POST) // 회원 가입
+	public String InsertMember(MemberVO mVO,MemberDAO mDAO) {
+		mDAO.insertMember(mVO);
+		return "redirect:login.do";	// 회원가입 후 로그인 페이지로 이동
+	}
+	
+	@RequestMapping(value="/updateM.do", method=RequestMethod.POST) // 회원정보 수정
+	public String UpdateMember(HttpSession session, MemberVO mVO,MemberDAO mDAO) {
+		mDAO.updateMember(mVO);					
+		session.invalidate();
+		return "redirect:login.do"; //수정후 다시 로그인을 하게 로그인 페이지로
+	}
+	
+	@RequestMapping(value="/deleteM.do") // 회원 탈퇴 
+	public String DeleteMember(HttpSession session, MemberVO mVO,MemberDAO mDAO) {
+		mDAO.deleteMember(mVO);
+		session.invalidate();
+		return "redirect:login.do"; // 회원 탈퇴를 누르면 다시 로그인 페이지로 이동
+	}
+		
 }
--- SpringShoes/src/main/java/com/ss/biz/controller/ReviewController.java
+++ SpringShoes/src/main/java/com/ss/biz/controller/ReviewController.java
@@ -1,5 +1,33 @@
 package com.ss.biz.controller;
 
-public class ReviewController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ss.biz.member.MemberVO;
+import com.ss.biz.review.ReviewVO;
+import com.ss.biz.review.impl.ReviewDAO;
+
+
+@Controller
+public class ReviewController {  
+	
+	@RequestMapping(value="/insertR.do",method=RequestMethod.POST) //후기 작성
+	public String InsertR(MemberVO mVO, ReviewVO rVO, ReviewDAO rDAO ) {
+		rDAO.insertReview(rVO);
+		return "review.do"; //작성을 누르면 review 페이지로 이동
+	}
+	
+	@RequestMapping("/updateR.do") //후기 수정
+	public String UpdateR(MemberVO mVO, ReviewVO rVO, ReviewDAO rDAO ) {
+		rDAO.updateReview(rVO);
+		return "review.do"; //수정을 누르면 review 페이지로 이동
+	}
+	
+	@RequestMapping("/deleteR.do") //후기 삭제
+	public String DeleteR(MemberVO mVO, ReviewVO rVO, ReviewDAO rDAO ) {
+		rDAO.deleteReview(rVO);
+		return "review.do"; //삭제를 누르면 review 페이지로 이동
+	}
+	
 }
