3c904538c5988755471577e3ed217fd12528a4c8
dusdn022 <dusdn0111@naver.com>
2022-10-02 16:30:47 +0900

    Pay, Address Controller 작성, shoes는 보완해서 올릴 예정

--- SpringShoes/src/main/java/com/ss/biz/controller/PayController.java
+++ SpringShoes/src/main/java/com/ss/biz/controller/PayController.java
@@ -1,5 +1,29 @@
 package com.ss.biz.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ss.biz.pay.PayVO;
+import com.ss.biz.pay.impl.PayDAO;
+
+@Controller
 public class PayController {
 
+	@RequestMapping(value="/insertP.do", method=RequestMethod.POST) //구매 목록 추가
+	public String insertPay(PayVO pVO, PayDAO pDAO) {
+		pDAO.insertPay(pVO);
+		return "cart.do"; 
+	}
+		
+	@RequestMapping(value="/selectAllPay.do", method=RequestMethod.POST) //모든 구매목록 보기
+	public String selectAllPay(PayVO pVO, PayDAO pDAO, Model model) {
+		List<PayVO> pDatas = pDAO.selectAllPay(pVO);
+		model.addAttribute("pDatas", pDatas);
+		return "cart.do";
+	}
+	
 }
